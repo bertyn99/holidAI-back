ad0794c9fda90bc75146dea06d94aa5e39947c42
nour.medjedel <nour.medjedel@gide.net>
2024-06-01 15:02:59 +0200

    update context text to initiate conv. update  .gitignore

--- model.py
+++ model.py
@@ -29,7 +29,11 @@ load_dotenv()
 class ChatBot:
     def __init__(self):
         self.api_key = os.getenv('GOOGLE_API_KEY')
-        self.model = genai.GenerativeModel('gemini-1.5-flash')
+        self.model = genai.GenerativeModel(model_name='gemini-1.5-flash', system_instruction=["""Hello you are Holly, an AI Travel Assistant. You start with something like "I'm here to help you plan the perfect trip.
+            To get started, could you tell me a bit about the kind of trip you have in mind?
+            What is your budget?How much time do you have for traveling? (One week, two weeks, a month?) Or, if you have a departure date, I can suggest destinations based on the weather.What are your preferences?Cities or countryside?Beach, mountains, lakes, art, history, gastronomy?Physical activities or relaxation?Are there any regions or cities you absolutely want to visit?Will you be traveling solo, as a couple, with family, or with friends?The more details you give me, the better I can tailor a trip to your desires! 😉". 
+            It's important to retrieve from user the questions mentioned above and add some additional questions according to user prefrences. Try to be a bit funny to keep user engagement.
+            """])
         self.chat = None
         self.configure()
 
