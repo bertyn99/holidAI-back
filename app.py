5567d320e731d55fd8799a412b0be80ea9acf92a
AmeniLefi <ameni.lefi@esprit.tn>
2024-06-02 03:48:48 +0200

    summary

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ def chat():
 @app.route('/upload_file', methods=['POST'])
 def upload_file():
   current_directory = os.getcwd()
-  directory = os.path.join(current_directory , "test2.png")
-  sample_file = genai.upload_file(path = directory , display_name ="voyage" )
+  direcory = os.path.join(current_directory , "test2.png")
+  sample_file = genai.upload_file(path = direcory , display_name ="voyage" )
   print(sample_file)
   response = chatbot.model.generate_content(["describe the image" , sample_file])
   chatbot.chat.history.append({
@@ -41,5 +41,24 @@ def upload_file():
     "parts":[{"text":response.text}]})
   return (response.text)
   
+
+@app.route('/summarize', methods=['GET']) 
+def summarize():
+
+  msg = ""
+  for i in range(len(chatbot.chat.history)):
+    msg += chatbot.chat.history[i].parts[0].text + " "
+
+  response = chatbot.get_json(
+      msg
+    )
+  map_response = response
+  
+  summary_response = chatbot.send_message(f"Please summarize JSON the following itinerary: {map_response}") 
+ 
+  summary_text = summary_response._result.candidates[0].content.parts[0].text
+  summarized_response = { "summary": summary_text, "details": map_response } 
+  return jsonify(summarized_response)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=False)
