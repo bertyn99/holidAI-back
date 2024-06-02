fe87a220d60ae66cb0ffbdba9cafd809b205cf81
clement <chaban.clement@gmail.com>
2024-06-02 13:31:13 +0200

    remove local file

--- app.py
+++ app.py
@@ -45,6 +45,8 @@ def upload_file():
   direcory = os.path.join(current_directory , file.filename)
   sample_file = genai.upload_file(path = direcory , display_name ="voyage" )
   print(sample_file)
+  if os.path.exists(direcory):
+    os.remove(direcory)
   response = chatbot.model.generate_content(["Ask questions about the travel the user want to make taking into account the image" , sample_file])
   chatbot.chat.history.append({
     "role":"user" ,
